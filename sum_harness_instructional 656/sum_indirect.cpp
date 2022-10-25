ea3835a3de07c2b3ce728f54ddd1823c164e4076
mason tsai <39562999+masontsaii@users.noreply.github.com>
2022-10-25 12:33:14 -0700

    Update sum_indirect.cpp

--- sum_harness_instructional 656/sum_indirect.cpp
+++ sum_harness_instructional 656/sum_indirect.cpp
@@ -11,38 +11,25 @@ uint64_t B[0];
 void
 setup(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_indirect problem_setup, N=%lld \n", N);
-
-   // Setting up A vector or array with length N filled
+    // Setting up A vector or array with length N filled
+    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
     for(int i = 0;i<N;i++)
     {
-        A[i] = i;
+        A[i] = rand() % N;
     }
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_indirect perform_sum, N=%lld \n", N);
 
-   int sum = 0;
-   std::vector<int> B;
-
-   // Generate random vector of N length
-   for (int i=0; i < N-1; i++) {
-       B.push_back(rand());
-   }
-
-   int indx = B[0];
-
-    for (int j=0; j < N; j++) {
+    printf(" inside sum_indirect perform_sum, N=%lld \n", N);
+    int sum = 0;
+    int indx = B[0];
+    for(int i=0;i<N;i++)
+    {
         sum += A[indx];
         indx = B[indx];
     }
-    std::cout << "sum = " << sum;
-
-
-   return sum;
+    return sum;
 }
-
-
